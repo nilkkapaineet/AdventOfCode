9e5851f15a3a68e56e43a0bcc7ffbca294ab62f9
nilkkapaineet <logintili@gmail.com>
2016-12-20 15:47:54 +0200

    Add files via upload
    Advent of Code, Day 20, Part 2
    Find legal IPs, values outside given range
    Very simple change from part 1

--- Main.java
+++ Main.java
@@ -1,191 +1,131 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.*;
-import java.util.HashMap;
-import java.awt.Component;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        List<String> data3 = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader("cal6.txt"))) {
-            //  FileReader fileReader = new FileReader(filename);
-            //   BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+        // write your code here
+        int counter = 0;
+        List<String> file = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader("testi.txt"))) {
             String line = null;
             while ((line = br.readLine()) != null) {
-                data3.add(line);
+                file.add(line);
+                counter++;
             }
             br.close();
-            data3.toArray(new String[data3.size()]);
+            file.toArray(new String[file.size()]);
         } catch(IOException e) {
             e.printStackTrace();
         }
-        String[] data = new String[data3.size()];
-        data = data3.toArray(data);
 
-      ArrayList al1 = new ArrayList();
-        ArrayList al2 = new ArrayList();
-        ArrayList al3 = new ArrayList();
-        ArrayList al4 = new ArrayList();
-        ArrayList al5 = new ArrayList();
-        ArrayList al6 = new ArrayList();
-        ArrayList al7 = new ArrayList();
-        ArrayList al8 = new ArrayList();
-
-        for(int i=0; i<data.length; i++) {
-           al1.add(data[i].substring(0,1));
-       }
-        for(int i=0; i<data.length; i++) {
-            al2.add(data[i].substring(1,2));
-        }
-        for(int i=0; i<data.length; i++) {
-            al3.add(data[i].substring(2,3));
-        }
-        for(int i=0; i<data.length; i++) {
-            al4.add(data[i].substring(3,4));
-        }
-        for(int i=0; i<data.length; i++) {
-            al5.add(data[i].substring(4,5));
-        }
-        for(int i=0; i<data.length; i++) {
-            al6.add(data[i].substring(5,6));
-        }
-        for(int i=0; i<data.length; i++) {
-            al7.add(data[i].substring(6,7));
-        }
-        for(int i=0; i<data.length; i++) {
-            al8.add(data[i].substring(7,8));
+        Map<Long, Long> map = new HashMap<Long, Long>();
+        // low range - high range
+        long[][] ranges = new long[counter][2];
+        int i = 0;
+        for (String input : file) {
+            long[] numbers = getNumbers(input);
+            ranges[i][0] = numbers[0];
+            ranges[i][1] = numbers[1];
+            i++;
+            map.put(numbers[0], numbers[1]);
         }
 
-        String str1 = al1.get(0).toString();
-        for(int i=0;i<al1.size();i++) {
-            if (i>0) {
-                str1 = str1.concat(al1.get(i).toString());
-            }
-        }
+        // System.out.println("\nSorted Map......By Value");
+        Map<Long, Long> valueMap = sortByValue(map);
+        // printMap(valueMap);
 
-        String str2 = al2.get(0).toString();
-        for(int i=0;i<al2.size();i++) {
-            if (i>0) {
-                str2 = str2.concat(al2.get(i).toString());
-            }
-        }
+        // System.out.println("\nSorted Map......By Key");
+        Map<Long, Long> keyMap = new TreeMap<Long, Long>(map);
+        // printMap(keyMap);
+        System.out.println("Searching...");
 
-        String str3 = al3.get(0).toString();
-        for(int i=0;i<al3.size();i++) {
-            if (i>0) {
-                str3 = str3.concat(al3.get(i).toString());
-            }
-        }
-
-        String str4 = al4.get(0).toString();
-        for(int i=0;i<al4.size();i++) {
-            if (i>0) {
-                str4 = str4.concat(al4.get(i).toString());
-            }
-        }
-
-        String str5 = al5.get(0).toString();
-        for(int i=0;i<al5.size();i++) {
-            if (i>0) {
-                str5 = str5.concat(al5.get(i).toString());
-            }
-        }
-
-        String str6 = al6.get(0).toString();
-        for(int i=0;i<al6.size();i++) {
-            if (i>0) {
-                str6 = str6.concat(al6.get(i).toString());
-            }
-        }
-
-        String str7 = al7.get(0).toString();
-        for(int i=0;i<al7.size();i++) {
-            if (i>0) {
-                str7 = str7.concat(al7.get(i).toString());
-            }
-        }
-
-        String str8 = al8.get(0).toString();
-        for(int i=0;i<al8.size();i++) {
-            if (i>0) {
-                str8 = str8.concat(al8.get(i).toString());
-            }
-        }
-
-        System.out.println(countChars(str1));
-        System.out.println(countChars(str2));
-        System.out.println(countChars(str3));
-        System.out.println(countChars(str4));
-        System.out.println(countChars(str5));
-        System.out.println(countChars(str6));
-        System.out.println(countChars(str7));
-        System.out.println(countChars(str8));
-
-    }
-
-    public static Character countChars(String str) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : str.toCharArray()) {
-            if (map.containsKey(ch)) {
-                int val = map.get(ch);
-                map.put(ch, val + 1);
+        long legalIPs = 0;
+        Map.Entry<Long, Long> lowest=keyMap.entrySet().iterator().next();
+        long veryLow = lowest.getKey();
+        long veryHigh = lowest.getValue();
+        for (Map.Entry<Long, Long> entry : keyMap.entrySet()) {
+            long low = entry.getKey();
+            long high = entry.getValue();
+            if ((veryHigh+1.1) <= low) {
+                // add to legal IPs
+                long temp = low-veryHigh-1;
+                legalIPs += temp;
+                if (veryHigh <= high) {
+                    veryHigh = high;
+                }
             } else {
-                map.put(ch, 1);
+                if (veryHigh <= high) {
+                    veryHigh = high;
+                }
             }
         }
-        // Display elements
-        // Get a set of the entries
-        Set set = map.entrySet();
-        // Get an iterator
-        Iterator i = set.iterator();
-        while(i.hasNext()) {
-            Map.Entry me = (Map.Entry)i.next();
-            /*
-            System.out.print(me.getKey() + ": ");
-            System.out.println(me.getValue());
-            */
-        }
 
-        Map.Entry<Character,Integer> maxEntry = null;
+        System.out.println("Range: Low: " + veryLow + " High: " + veryHigh + " So, found: " + (veryHigh+1));
+        System.out.println("Legal IPs: " + legalIPs);
 
-        for(Map.Entry<Character,Integer> entry : map.entrySet()) {
-            if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
-                maxEntry = entry;
-            }
-        }
-// maxEntry should now contain the maximum,
-
-        return maxEntry.getKey(); // maxEntry.getValue();
     }
 
-    public static String[] readLines(String filename) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-          //  FileReader fileReader = new FileReader(filename);
-         //   BufferedReader bufferedReader = new BufferedReader(fileReader);
-            List<String> lines = new ArrayList<String>();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);
-            }
-            br.close();
-            return lines.toArray(new String[lines.size()]);
-        } catch(IOException e) {
-            e.printStackTrace();
+    public static <K, V> void printMap(Map<K, V> map) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey()
+                    + " Value : " + entry.getValue());
         }
-        return null;
     }
 
+    public static long[] getNumbers(String input) {
+        // input type: digit1-digit2
+        Pattern p = Pattern.compile("-?\\d+");
+        Matcher m = p.matcher(input);
+        boolean found = false;
+        long[] rectangle = new long[2];
+        while (m.find()) {
+            if (!found) {
+                rectangle[0] = Long.parseLong(m.group());
+                found = true;
+            } else {
+                rectangle[1] = Math.abs(Long.parseLong(m.group()));
+            }
+        }
+        return rectangle;
+    }
+
+    private static Map<Long, Long> sortByValue(Map<Long, Long> unsortMap) {
+
+        // 1. Convert Map to List of Map
+        List<Map.Entry<Long, Long>> list =
+                new LinkedList<Map.Entry<Long, Long>>(unsortMap.entrySet());
+
+        // 2. Sort list with Collections.sort(), provide a custom Comparator
+        //    Try switch the o1 o2 position for a different order
+        Collections.sort(list, new Comparator<Map.Entry<Long, Long>>() {
+            public int compare(Map.Entry<Long, Long> o1,
+                               Map.Entry<Long, Long> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<Long, Long> sortedMap = new LinkedHashMap<Long, Long>();
+        for (Map.Entry<Long, Long> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        /*
+        //classic iterator example
+        for (Iterator<Map.Entry<String, Long>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<String, Long> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }*/
+
+        return sortedMap;
+    }
 }
